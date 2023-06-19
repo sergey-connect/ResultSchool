3ff2a29246019316c5f9040e821a12f284644d89
sergei-connect <fresh787@yandex.ru>
2023-06-19 17:58:08 +0200

    Марафон по JS ResultSchool

---
+++ marathon-urok-4/app.js
@@ -0,0 +1,31 @@
+const board = document.querySelector('#board')
+const colors = ['#e23320', '#9f31ce', '#056baf', '#f0ec15', '#2de078', '#3d20e2', '#c5c476']
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', setColor)
+
+    square.addEventListener('mouseleave', removeColor)
+
+    board.append(square)
+}
+
+function setColor(event) {
+    const element = event.target
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
