a07749ef081177fda26a50aa3cf5febbd86e5220
sergei-connect <fresh787@yandex.ru>
2023-06-29 19:02:53 +0200

    Основы JS

---
+++ 01-base/theory.js
@@ -0,0 +1,38 @@
+// ============= Operators
+let a = 10
+let b = 5
+let c = 32
+
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// с = 4 ** 2
+
+// ============== Data types
+const age = 29 // number
+const name = 'Sergey' // string
+const isProgrammer = true // boolean
+let x // undefined
+// null
+// NaN - number
+console.log(typeof x)
+
+
+// ============== Priority
+const fullAge = 29
+const birthYear = 1993
+const currentYear = 2023
+// > < >= <=
+const isFullAge = currentYear - birthYear >= fullAge
+
+// console.log(isFullAge)
+
+const num1 = 42
+const num2 = '42'
