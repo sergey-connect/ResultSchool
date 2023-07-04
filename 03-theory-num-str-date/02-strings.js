1420602fb1671aba8a33559e9eb0493377c1b488
sergei-connect <fresh787@yandex.ru>
2023-07-04 23:34:25 +0200

    Теория JS Строки, числа и функции

---
+++ 03-theory-num-str-date/02-strings.js
@@ -0,0 +1,24 @@
+const firstName = 'Sergey'
+const age = "29"
+
+// function getAge() {
+//     return age
+// }
+
+// // const old = 'Hello my name is ' + name + ' and my age is ' + age
+// const output = `Hello my name is ${name} and my age is ${getAge() > 18 ? 'Can drive' : 'Cant drive'}`
+
+
+console.log(firstName.length);
+console.log(firstName);
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+console.log(firstName.charAt(2));
+console.log(firstName.indexOf('!'));
+console.log(firstName.toLowerCase().startsWith('se'));
+console.log(firstName.endsWith('ey!'));
+console.log(firstName.repeat(2));
+
+const password = '     my super pass     '
+
+console.log(password.trim()); // Обрезка пробелов
