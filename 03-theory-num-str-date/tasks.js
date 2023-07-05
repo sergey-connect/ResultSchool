b625b4078a545d9e74b4c9114e230dc1b765bc77
sergei-connect <fresh787@yandex.ru>
2023-07-05 13:12:17 +0200

    Теоретическая база. Задачи - часть 2

--- 03-theory-num-str-date/tasks.js
+++ 03-theory-num-str-date/tasks.js
@@ -50,10 +50,10 @@ console.log(func(8.5, 1, 10)); // 8
 /*
 Задание #3
 
-Создайте функцию isPerfectNumber(), которая принимает целое положительное число в параметры и возвращает true, 
+Создайте функцию isPerfectNumber(), которая принимает целое положительное число в параметры и возвращает true,
 если число является совершенным, и false в противном случае.
 
-Примечание. Совершенное число — это число, равное сумме всех своих положительных делителей (за исключением самого числа). 
+Примечание. Совершенное число — это число, равное сумме всех своих положительных делителей (за исключением самого числа).
 Например, число 6 является совершенным, потому что делители числа 6 (1, 2, 3) в сумме дают 6.
 
 function isPerfectNumber(number) {
@@ -84,10 +84,10 @@ console.log(isPerfectNumber(16));  // false
 /*
 Задание #4
 
-Напишите функцию getNumberDigit(), которая принимает число (number) и индекс цифры в числе (digitPosition). 
-Возвращает она цифру числа number, находящуюся на позиции digitPosition. 
+Напишите функцию getNumberDigit(), которая принимает число (number) и индекс цифры в числе (digitPosition).
+Возвращает она цифру числа number, находящуюся на позиции digitPosition.
 
-Если происходит попытка чтения числа на позиции, которой не существует (например, кол-во цифр в числе меньше, 
+Если происходит попытка чтения числа на позиции, которой не существует (например, кол-во цифр в числе меньше,
     чем передано в digitPosition), то верните undefined.
 
 Примечание. Нельзя использовать приведение числа к строке.
@@ -103,27 +103,27 @@ getNumberDigit(1, 2) // undefined
 */
 
 
-function getNumberDigit(number, digitPosition) {
-    let maxIndex = -1
-    let digit;
-    let tempNum = number
+// function getNumberDigit(number, digitPosition) {
+//     let maxIndex = -1
+//     let digit;
+//     let tempNum = number
 
-    while (Math.trunc(tempNum) !== 0) {
-        Math.round(tempNum /= 10)
-        maxIndex++
-    }
+//     while (Math.trunc(tempNum) !== 0) {
+//         Math.round(tempNum /= 10)
+//         maxIndex++
+//     }
 
-    if (digitPosition > maxIndex) return undefined;
+//     if (digitPosition > maxIndex) return undefined;
 
-    number /= 10 ** (maxIndex + 1)
+//     number /= 10 ** (maxIndex + 1)
 
-    for (let i = 0; i <= digitPosition; i++) {
-        number *= 10
-        digit = number.toFixed(maxIndex) % 10
-    }
+//     for (let i = 0; i <= digitPosition; i++) {
+//         number *= 10
+//         digit = number.toFixed(maxIndex) % 10
+//     }
 
-    return Math.trunc(digit)
-}
+//     return Math.trunc(digit)
+// }
 
 /*
 function getNumberDigit(number, digitPosition) {
@@ -147,7 +147,158 @@ function getNumberDigit(number, digitPosition) {
 }
 */
 
-console.log(getNumberDigit(123, 0)) // 1
-console.log(getNumberDigit(123, 1)) // 2
-console.log(getNumberDigit(35791, 0)) // 3
-console.log(getNumberDigit(1, 2)) // undefined
+// console.log(getNumberDigit(123, 0)) // 1
+// console.log(getNumberDigit(123, 1)) // 2
+// console.log(getNumberDigit(35791, 0)) // 3
+// console.log(getNumberDigit(1, 2)) // undefined
+
+
+// ================== Часть 2 ==============
+
+/*
+Задание #1
+
+Дан объект машины.
+
+const car = {
+  name: 'Hendai Solaris',
+    type: 'sedan',
+    maxSpeed: '320',
+    color: 'red',
+}
+Необходимо написать написать функцию, которая принимает объект машины и возвращает строку с HTML, заполненную значениями переданного объекта.
+
+<article>
+    <div>
+        <h2>Hendai Solaris</h2>
+        <span>sedan</span>
+    </div>
+    <div>
+        <span>Maximum speed:</span>
+        <span>320</span>
+    </div>
+    <div>
+        <span>Color:</span>
+        <span>red</span>
+    </div>
+</article>
+*/
+
+// const car = {
+//     name: 'Hendai Solaris',
+//     type: 'sedan',
+//     maxSpeed: '320',
+//     color: 'red',
+// }
+
+// function makeHTML(obj) {
+//     return `<article>
+//     <div>
+//         <h2>${obj.name}</h2>
+//         <span>${obj.type}</span>
+//     </div>
+//     <div>
+//         <span>Maximum speed:</span>
+//         <span>${obj.maxSpeed}</span>
+//     </div>
+//     <div>
+//         <span>Color:</span>
+//         <span>${obj.color}</span>
+//     </div>
+// </article>`
+// }
+
+// console.log(makeHTML(car))
+
+
+/*
+Задание #2
+
+Необходимо написать функцию countWords(), которая принимает предложение и подсчитывает количество слов в нем.
+
+const sentence = "Hello, how are you?";
+console.log(countWords(sentence)); // 4
+*/
+
+// function countWords(sent) {
+//     return sent.split(' ').length
+// }
+
+// const sentence = "Hello, how are you? What are you doing now?";
+// console.log(countWords(sentence)); // 4
+
+
+/*
+Задание #3
+
+Необходимо написать функцию getInitials(), которая принимает полное имя в виде строки (например, “John Doe”) и возвращает инициалы в формате “J. D.”.
+
+const fullName = "John Doe";
+const initials = getInitials(fullName);
+console.log(initials); // "J. D."
+*/
+
+// function getInitials(name) {
+//     const initials = name.split(' ').map(s => s[0] + '.')
+//     return initials.join(' ')
+// }
+
+// const fullName = "John Doe";
+// const initials = getInitials(fullName);
+// console.log(initials); // "J. D."
+
+
+/*
+Задание #4
+
+Необходимо написать функцию generateGoogleString(), которая принимает число и возвращает сроку ”Google”
+с указанным количеством букв “o”. При этом минимальное количество букв “о” равно 2.
+
+console.log(generateGoogleString(4));
+// Goooogle
+
+console.log(generateGoogleString(0));
+// Google
+
+console.log(generateGoogleString(-2));
+// Google
+*/
+
+// function generateGoogleString(count) {
+//     count <= 2 ? count = 2 : count = count
+//     return `g${'o'.repeat(count)}gle`
+// }
+
+// console.log(generateGoogleString(10));
+// // Goooogle
+
+// console.log(generateGoogleString(5));
+// // Google
+
+// console.log(generateGoogleString(-2));
+// // Google
+
+
+/*
+Задание #5
+
+Реализуйте функцию countVowels(), которая подсчитывает количество гласных букв 
+(латинских и кириллических) в переданной строке. 
+
+const str = "Hello, Привет!";
+console.log(countVowels(str)); // 4
+*/
+
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(str[i].toLowerCase())) counter += 1
+    }
+
+    return counter
+}
+
+const str = "Hello world, Привет мир! Ура!";
+console.log(countVowels(str)); // 4
