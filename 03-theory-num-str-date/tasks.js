0d10565d511e3b3278e103b5569ca328526ebb6c
sergei-connect <fresh787@yandex.ru>
2023-07-05 14:05:31 +0200

    Теоретическая база. Задачи - часть 3

--- 03-theory-num-str-date/tasks.js
+++ 03-theory-num-str-date/tasks.js
@@ -282,23 +282,226 @@ console.log(generateGoogleString(-2));
 /*
 Задание #5
 
-Реализуйте функцию countVowels(), которая подсчитывает количество гласных букв 
-(латинских и кириллических) в переданной строке. 
+Реализуйте функцию countVowels(), которая подсчитывает количество гласных букв
+(латинских и кириллических) в переданной строке.
 
 const str = "Hello, Привет!";
 console.log(countVowels(str)); // 4
 */
 
-function countVowels(str) {
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
-    let counter = 0;
+// function countVowels(str) {
+//     const vowels = ['a', 'e', 'i', 'o', 'u', 'y', 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+//     let counter = 0;
 
-    for (let i = 0; i < str.length; i++) {
-        if (vowels.includes(str[i].toLowerCase())) counter += 1
-    }
+//     for (let i = 0; i < str.length; i++) {
+//         if (vowels.includes(str[i].toLowerCase())) counter += 1
+//     }
 
-    return counter
+//     return counter
+// }
+
+// const str = "Hello world, Привет мир! Ура!";
+// console.log(countVowels(str)); // 4
+
+
+// ============== Часть 3 ===========
+
+/*
+Задание #1
+
+Напишите функцию sumNumbers(), которая принимает произвольное количество чисел через запятую и возвращает их сумму.
+
+const result1 = sumNumbers(1, 2, 3, 4, 5);
+console.log(result1); // 15
+
+const result2 = sumNumbers(10, 20, 30);
+console.log(result2); // 60
+*/
+
+// function sumNumbers(...numbers) {
+//     return numbers.reduce((acc, n) => acc + n, 0)
+// }
+
+// const result1 = sumNumbers(1, 2, 3, 4, 5);
+// console.log(result1); // 15
+
+// const result2 = sumNumbers(10, 20, 30);
+// console.log(result2); // 60
+
+
+/*
+Задание #2
+
+Напишите функцию findMaxValue(), которая принимает массив чисел и находит максимальное значение.
+Если массив пустой, то возвращается значение undefined.
+
+const numbers1 = [1, 2, 3, 4, 5];
+const max1 = findMaxValue(numbers1);
+console.log(max1); // 5
+
+const numbers2 = [10, 20, 5, 30, 15];
+const max2 = findMaxValue(numbers2);
+console.log(max2); // 30
+
+const emptyArray = [];
+const maxEmpty = findMaxValue(emptyArray);
+console.log(maxEmpty); // undefined
+*/
+
+// function findMaxValue(numbers) {
+//     if (numbers.length === 0) return undefined
+//     let max = numbers[0];
+//     for (let i = 1; i < numbers.length; i++) {
+//         if (numbers[i] > max) max = numbers[i]
+//     }
+
+//     return max
+// }
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// const max1 = findMaxValue(numbers1);
+// console.log(max1); // 5
+
+// const numbers2 = [10, 20, 5, 30, 15];
+// const max2 = findMaxValue(numbers2);
+// console.log(max2); // 30
+
+// const emptyArray = [];
+// const maxEmpty = findMaxValue(emptyArray);
+// console.log(maxEmpty); // undefined
+
+
+/*
+Задание #3
+
+Напишите функцию calculateAverage(), которая принимает массив чисел и вычисляет их среднее арифметическое.
+Если массив пустой, то возвращается 0.
+
+const numbers1 = [1, 2, 3, 4, 5];
+console.log(calculateAverage(numbers1)); // 3
+
+const numbers2 = [10, 20, 30, 40, 50];
+console.log(calculateAverage(numbers2)); // 30
+
+const emptyArray = [];
+console.log(calculateAverage(emptyArray)); // 0
+*/
+
+// function calculateAverage(numbers) {
+//     if (numbers.length === 0) return 0
+
+//     return numbers.reduce((acc, n) => acc + n, 0) / numbers.length
+// }
+
+// const numbers1 = [1, 2, 3, 4, 5];
+// console.log(calculateAverage(numbers1)); // 3
+
+// const numbers2 = [10, 20, 30, 40, 50];
+// console.log(calculateAverage(numbers2)); // 30
+
+// const emptyArray = [];
+// console.log(calculateAverage(emptyArray)); // 0
+
+
+/*
+Задание #4
+
+Напишите функцию isPalindrome(), которая принимает строку и возвращает true, если она является палиндромом, иначе — false.
+
+Примечание. Палиндром — строка, которая читается одинаково слева направо и справа налево.
+
+console.log(isPalindrome('level')); // true
+console.log(isPalindrome('radar')); // true
+console.log(isPalindrome('hello')); // false
+*/
+
+// function isPalindrome(str) {
+//     let strRev = str.split('').reverse().join('')
+//     return str === strRev
+// }
+
+// console.log(isPalindrome('level')); // true
+// console.log(isPalindrome('radar')); // true
+// console.log(isPalindrome('hello')); // false
+
+
+/*
+Задание #5
+
+Напишите функцию removeDuplicates(), которая принимает массив и возвращает новый массив без дубликатов.
+
+const numbers1 = [1, 2, 3, 3, 4, 5, 5];
+console.log(removeDuplicates(numbers1));  // [1, 2, 3, 4, 5]
+
+const numbers2 = [10, 20, 30, 30, 40, 40, 50];
+console.log(removeDuplicates(numbers2));  // [10, 20, 30, 40, 50]
+
+const emptyArray = [];
+console.log(removeDuplicates(emptyArray));  // []
+*/
+
+// function removeDuplicates(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         for (let j = i + 1; j < array.length; j++) {
+//             if (array[i] === array[j]) {
+//                 array.splice(j, 1)
+//                 j
+//             }
+//         }
+//     }
+
+//     return array
+// }
+
+// const numbers1 = [1, 2, 3, 3, 4, 5, 5];
+// console.log(removeDuplicates(numbers1));  // [1, 2, 3, 4, 5]
+
+// const numbers2 = [10, 20, 30, 30, 40, 40, 50];
+// console.log(removeDuplicates(numbers2));  // [10, 20, 30, 40, 50]
+
+// const emptyArray = [];
+// console.log(removeDuplicates(emptyArray));  // []
+
+
+/*
+Задание #6
+
+Создайте функцию createCounter(), которая принимает начальное значение и возвращает три функции:
+
+inc() — увеличивает значение на 1;
+dec() — уменьшает значение на 1;
+get() — возвращает текущее значение.
+Пример использования функции:
+
+function createCounter(value) {
+    // Ваш код здесь...
 }
 
-const str = "Hello world, Привет мир! Ура!";
-console.log(countVowels(str)); // 4
+const { inc, dec, get } = createCounter(5);
+console.log(get()); // 5
+inc();
+inc();
+inc();
+dec();
+console.log(get()); // 7
+*/
+
+function createCounter(value) {
+    return {
+        inc() {
+            return value++
+        }, dec() {
+            return value--
+        }, get() {
+            return value
+        },
+    }
+}
+
+const { inc, dec, get } = createCounter(5);
+console.log(get()); // 5
+inc();
+inc();
+inc();
+dec();
+console.log(get()); // 7
